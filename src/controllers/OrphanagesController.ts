bd54abbd59e80f31616830cecac6df8a1a727fd8
Gabriel Juliano <gabriel.oliveira8@hotmail.com>
2020-10-30 11:26:00 -0300

    Casting para boolean

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -41,7 +41,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends,
+            open_on_weekends: open_on_weekends === 'true',
             images
         }
 
@@ -60,6 +60,8 @@ export default {
             )
 
         });
+
+        const finalData = schema.cast(data);
 
         await schema.validate(data,{
             abortEarly:false,
